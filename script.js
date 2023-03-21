30ab293b59f55a219a52e3edd45a28b5c030b103
Ely Carvalho <elycarvalho3@gmail.com>
2023-03-21 05:24:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const braco2 = document.querySelector('.braco2')
 const pinca = document.querySelector('.pinca')
 const pontaPinca = document.querySelector('.ponta-pinca1')
 const pontaPinca2 = document.querySelector('.ponta-pinca2')
+const led = document.querySelector('.led')
 let movimentoB1 = 0
 let movimentoB2 = 90
 let movimentoPinca = 0
 let movimentoPonta = 5
-let movimentoPonta2 = 60
+let movimentoPonta2 = 65
+const objeto = document.querySelector('.objeto')
 
 function moveBraco1(movimento){
+    if(movimentoB2 > 90 && movimentoB1 > 18) {movimentoB1 = 18}
     if(movimento === 'desce'){
+        
+        if(movimentoB1 > 32) {movimentoB1 = 32}
         movimentoB1 = movimentoB1 + 2
         braco1.style.transform = `rotate(${movimentoB1}deg)`
         
@@ -18,20 +23,21 @@ function moveBraco1(movimento){
     }
     if(movimento === 'sobe'){
         movimentoB1 = movimentoB1 - 2
+        if(movimentoB1  < -18) {movimentoB1 = -18}
         braco1.style.transform = `rotate(${movimentoB1}deg)`
-        
         console.log('braço 1 S ' + movimentoB1)
     }  
 }
 
 function moveBraco2(movimento){
     if(movimento === 'desce'){
+        if(movimentoB2 > 120) {movimentoB2 = 120}
         movimentoB2 = movimentoB2 + 2
         braco2.style.transform = `rotate(${movimentoB2}deg)`
-        console.log('braço 2 D ' + movimentoB2)
-        
+        console.log('braço 2 D ' + movimentoB2)  
     }
     if(movimento === 'sobe'){
+        if(movimentoB2 < 58) {movimentoB2 = 58}
         movimentoB2 = movimentoB2 - 2
         braco2.style.transform = `rotate(${movimentoB2}deg)`
         console.log('braço 2 S' + movimentoB2)
@@ -55,11 +61,14 @@ function movePonta(movimento){
     if(movimento === 'abre'){
         movimentoPonta--
         movimentoPonta2++   
-        if(movimentoPonta2 > 60) {movimentoPonta2 = 60}
+        if(movimentoPonta2 > 65) {movimentoPonta2 = 65}
         if(movimentoPonta < 5) {movimentoPonta = 5}     
         pontaPinca.style.left = `${movimentoPonta2}px`
         pontaPinca2.style.left = `${movimentoPonta}px`
-
+        
+        if(movimentoPonta2 > 56 && movimentoB2 === 56 && movimentoB1 === 34){
+            soltar()
+        }
         console.log('abre 1 ' + movimentoPonta)
         console.log('abre 2 ' + movimentoPonta2)
         
@@ -68,7 +77,11 @@ function movePonta(movimento){
         movimentoPonta++
         movimentoPonta2--
         if(movimentoPonta2 < 45) {movimentoPonta2 = 45}
-        if(movimentoPonta > 20) {movimentoPonta = 20}
+        if(movimentoPonta > 25) {movimentoPonta = 25}
+
+        if(movimentoPonta === 14 && movimentoB2 > 114){
+            capturar()
+        }
         pontaPinca.style.left = `${movimentoPonta2}px`
         pontaPinca2.style.left = `${movimentoPonta}px`
         console.log('fecha 1 ' + movimentoPonta)
@@ -77,7 +90,6 @@ function movePonta(movimento){
 }
 
 document.addEventListener('keydown', (e) => {
-    console.log(e.key)
     switch(e.key){
         case 'ArrowUp':
           moveBraco1('sobe')
@@ -106,3 +118,20 @@ document.addEventListener('keydown', (e) => {
             movePonta('abre')
     }
 })
+
+function capturar(){
+    if(movimentoB1 === 0 && movimentoB2 > 114 && movimentoPonta === 14){
+        objeto.remove()
+        objetoC = document.createElement('div')
+        objetoC.className = "objetoC"
+        pinca.appendChild(objetoC)
+        led.style.backgroundColor = '#51EC66'
+        console.log('capturou')
+        
+    } else {alert('fora da posição!')}
+}
+
+function soltar(){
+    
+        alert('soltar')
+}
